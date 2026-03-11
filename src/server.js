92d9664829081dd2b4177f1c217e65a7fea6b0fa
alexandre@routing.com.br <alexandre@routing.com.br>
2026-03-11 16:25:17 -0300

    mudanças back-end

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+console.log("SERVIDOR INICIADO");
 
 const app = express();
+app.use(cors());
+app.use((req, res, next) => {
+  console.log("Requisição recebida:", req.method, req.url);
+  next();
+});
 app.use(express.json());
+
 
 // 🔹 Conexão com MongoDB
 mongoose.connect(
